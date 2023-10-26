c0506f708b05e0645278c1684c26fba700830116
AramThalje <101216661+AramThalje@users.noreply.github.com>
2023-10-26 18:23:04 +0200

    Add files via upload

---
+++ src/main/java/org/example/LeapYear.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class LeapYear {
+    public static boolean isLeapYear(int year){
+        if ((year %4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
---
+++ src/test/java/testLeapYear.java
@@ -0,0 +1,38 @@
+import org.example.LeapYear;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class testLeapYear {
+
+    @Test
+    public void Test_If_LeapYear_Divisble_By_4_But_Not_By_100() {
+        assertTrue(LeapYear.isLeapYear(2000));
+        assertTrue(LeapYear.isLeapYear(2020));
+        assertTrue(LeapYear.isLeapYear(2400));
+
+
+    }
+    @Test
+    public void Test_If_LeapYear_Divisible_By_400() {
+        assertTrue(LeapYear.isLeapYear(2000));
+        assertTrue(LeapYear.isLeapYear(2400));
+    }
+
+
+
+    @Test
+    public void Test_If_Non_LeapYear() {
+        assertFalse(LeapYear.isLeapYear(1700));
+        assertFalse(LeapYear.isLeapYear(1800));
+        assertFalse(LeapYear.isLeapYear(1900));
+        assertFalse(LeapYear.isLeapYear(2018));
+
+        assertFalse(LeapYear.isLeapYear(2022));
+    }
+    @Test
+    public void testNonLeapYearIsNotDivisibleBy4(){
+        assertFalse(LeapYear.isLeapYear(2021));
+        assertFalse(LeapYear.isLeapYear(2022));
+        assertFalse(LeapYear.isLeapYear(2023));
+
+    }
+}
